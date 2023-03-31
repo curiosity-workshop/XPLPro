d5cb9783d35d691857c958ff84f6acf11f640df4
GioCC <g.crocic@gmail.com>
2023-03-31 13:58:30 +0200

    First Arduino draft

---
+++ XPLPro_Arduino/XPLPro.cpp
@@ -0,0 +1,507 @@
+/*
+  XPLPro.cpp 
+  Created by Curiosity Workshop, Michael Gerlicher, 2023.
+ 
+*/
+
+
+#include <arduino.h>
+#include "XPLPro.h"
+
+
+XPLPro::XPLPro(Stream* device)
+{
+	streamPtr = device;
+	streamPtr->setTimeout(XPL_RX_TIMEOUT);
+}
+
+void XPLPro::begin(char * devicename, void *initFunction, void *stopFunction)
+{
+		
+	_deviceName = devicename;
+	_connectionStatus = 0;
+	_receiveBuffer[0] = 0;
+	_registerFlag = 0;
+		
+	_xplInitFunction = initFunction;
+	_xplStopFunction = stopFunction;
+}
+
+
+
+int XPLPro::xloop(void)
+{
+	_processSerial();
+	return _connectionStatus;
+}
+
+int XPLPro::commandTrigger(int commandHandle)
+{
+	return commandTrigger(commandHandle, 1);
+
+}
+
+int XPLPro::commandTrigger(int commandHandle, int triggerCount)
+{
+	_sendPacketInt(XPLCMD_COMMANDTRIGGER, commandHandle, (long int)triggerCount);
+
+	return 0;
+}
+
+int XPLPro::commandStart(int commandHandle)
+{
+	
+	_sendPacketVoid(XPLCMD_COMMANDSTART, commandHandle);
+	return 0;
+
+}
+
+int XPLPro::commandEnd(int commandHandle)
+{
+	
+	_sendPacketVoid(XPLCMD_COMMANDEND, commandHandle);
+	return 0;
+
+}
+
+int XPLPro::connectionStatus()
+{
+	return _connectionStatus;
+
+}
+
+
+int XPLPro::sendDebugMessage(const char* msg)
+{
+	
+	_sendPacketString(XPLCMD_PRINTDEBUG, msg);
+
+	return 1;
+}
+
+int XPLPro::sendSpeakMessage(const char* msg)
+{
+
+	_sendPacketString(XPLCMD_SPEAK, msg);
+
+	return 1;
+}
+
+/*
+
+int XPLPro::hasUpdated(int handle)
+{
+	
+	return false;
+}
+
+int XPLPro::datarefsUpdated()
+{
+	
+	return false;
+}
+
+void XPLPro::datarefRead(int handle, long int * value)
+{
+	//if (_dataRefs[handle]->dataRefHandle >= 0 && (_dataRefs[handle]->dataRefRWType == XPL_READ || _dataRefs[handle]->dataRefRWType == XPL_READWRITE))
+	//    *value =  *(long int *)_dataRefs[handle]->latestValue;
+	
+}
+
+void XPLPro::datarefRead(int handle, float * value)
+{
+	//if (_dataRefs[handle]->dataRefHandle >= 0 && (_dataRefs[handle]->dataRefRWType == XPL_READ || _dataRefs[handle]->dataRefRWType == XPL_READWRITE))
+//		*value = *(float*)_dataRefs[handle]->latestValue;
+
+	
+}
+
+
+void XPLPro::datarefWrite(int handle, long int value)
+{
+	//if (_dataRefs[handle]->dataRefHandle >= 0 && (_dataRefs[handle]->dataRefRWType == XPL_WRITE || _dataRefs[handle]->dataRefRWType == XPL_READWRITE))
+	//	*(long int *)_dataRefs[handle]->latestValue = value;
+
+}
+
+void XPLPro::datarefWrite(int handle, float value)
+{
+	//if (_dataRefs[handle]->dataRefHandle >= 0 && (_dataRefs[handle]->dataRefRWType == XPL_READ || _dataRefs[handle]->dataRefRWType == XPL_READWRITE))
+//		*(float*)_dataRefs[handle]->latestValue = value;
+}
+
+*/
+
+void XPLPro::_sendname()
+{
+	if (_deviceName != NULL)
+	{
+		_sendPacketString(XPLRESPONSE_NAME, _deviceName);
+		_registerFlag = 1;
+	}
+		
+	//_xplInitFunction();						// Call the init function since we know xplane and the plugin are up and running
+
+}
+
+
+
+void XPLPro::sendResetRequest()
+{
+
+	if (_deviceName != NULL)
+	{
+		_sendPacketVoid(XPLCMD_RESET, 0);
+	}
+
+
+}
+
+void XPLPro::_processSerial()
+{
+
+
+	while (streamPtr->available() && _receiveBuffer[0] != XPL_PACKETHEADER)  _receiveBuffer[0] = (char)streamPtr->read();
+
+	if (_receiveBuffer[0] != XPL_PACKETHEADER) return;
+
+
+	_receiveBufferBytesReceived = streamPtr->readBytesUntil(XPL_PACKETTRAILER, (char*)&_receiveBuffer[1], XPLMAX_PACKETSIZE - 1);
+
+	if (_receiveBufferBytesReceived == 0)
+	{
+		_receiveBuffer[0] = 0;
+		return;
+	}
+
+
+	_receiveBuffer[++_receiveBufferBytesReceived] = XPL_PACKETTRAILER;
+	_receiveBuffer[++_receiveBufferBytesReceived] = 0;							// old habits die hard.  
+
+	// at this point we should have a valid frame
+	_processPacket();
+	
+}
+
+void XPLPro::_processPacket()
+{
+ 
+	if (_receiveBuffer[0] != XPL_PACKETHEADER) return;
+
+	switch (_receiveBuffer[1])
+	{
+		
+
+		case XPL_EXITING :					
+		
+			_connectionStatus = false;
+			_xplStopFunction();
+			break;
+		
+
+	    case XPLCMD_SENDNAME :
+		
+			_sendname();
+			_connectionStatus = true;			//not considered active till you know my name
+
+			break;
+		
+
+		case XPLCMD_SENDREQUEST :
+			_xplInitFunction();						// plugin is ready for registrations
+
+			break;
+
+		case XPLRESPONSE_DATAREF :
+		   
+			_parseInt(&_handleAssignment, _receiveBuffer, 2);
+
+			sendDebugMessage("dataref response received");
+			break;
+		
+
+		case XPLRESPONSE_COMMAND:
+		
+			_parseInt(&_handleAssignment, _receiveBuffer, 2);
+			sendDebugMessage("command response received");
+			break;
+		
+
+		
+		case XPLCMD_DATAREFUPDATE :
+		
+			//int refhandle = _getHandleFromFrame();
+						
+			break;
+		
+		
+		case XPLREQUEST_REFRESH:
+		
+
+			break;
+		
+
+		
+		default:
+		
+			
+			break;
+		
+
+	}
+
+	_receiveBuffer[0] = 0;
+}
+
+
+void XPLPro::_sendPacketInt(int command, int handle, long int value)			// for ints
+{
+	if (handle < 0) return;
+
+	//sprintf(_sendBuffer, "%c%c%3.3i%07.7i%c", XPLDIRECT_PACKETHEADER, command, handle, value, XPLDIRECT_PACKETTRAILER);
+	sprintf(_sendBuffer, "%c%c,%3.3i,%ld%c", XPL_PACKETHEADER, command, handle, value, XPL_PACKETTRAILER);
+//streamPtr->print("building packet: ");  streamPtr->println(_sendBuffer);
+	_transmitPacket();
+
+}
+
+void XPLPro::_sendPacketFloat(int command, int handle, float value)		// for floats
+{
+	if (handle < 0) return;
+// some boards cant do sprintf with floats so this is a workaround.  What a pain.  Implementing 2 decimals of precision for now.
+	//sprintf(_sendBuffer, "%c%c%3.3i%07i.%02i%c", 
+	  sprintf(_sendBuffer, "%c%c%3.3i0%i.%3.3i%c",					// change to reduce data flow 01/26/2021 MG
+																	// fix problem with leading zeros 03/01/2022
+		XPL_PACKETHEADER, 
+		command, 
+		handle, 
+		abs((int)value), 
+		abs((int)(value * 1000) % 1000), 
+		XPL_PACKETTRAILER);
+
+	if (value < 0) _sendBuffer[5] = '-';
+/*	
+Alternate method perhaps
+
+char tBuf[20];
+	dtostrf(value, 0, 2, tBuf);
+
+	sprintf(_sendBuffer, "%c%c%3.3i%s%c",
+		XPLDIRECT_PACKETHEADER,
+		command,
+		handle,
+		buff,
+		XPLDIRECT_PACKETTRAILER);
+
+		
+*/
+	_transmitPacket();
+	
+}
+
+void XPLPro::_sendPacketVoid(int command, int handle)			// just a command with a handle
+{
+	if (handle < 0) return;
+
+	sprintf(_sendBuffer, "%c%c%3.3i%c", XPL_PACKETHEADER, command, handle, XPL_PACKETTRAILER);
+  
+  _transmitPacket();
+}
+
+void XPLPro::_sendPacketString(int command, const char *str)			// for a string
+{
+	
+	
+	sprintf(_sendBuffer, "%c%c,\"%s\"%c", XPL_PACKETHEADER, command, str, XPL_PACKETTRAILER);
+
+	_transmitPacket();
+}
+
+
+void XPLPro::_transmitPacket(void)
+{
+	
+	streamPtr->write(_sendBuffer);
+	if (strlen(_sendBuffer) == 64) streamPtr->print(" ");			// apparantly a bug in arduino with some boards when we transmit exactly 64 bytes.  That took a while to track down...
+
+}
+
+
+int XPLPro::_parseString(char* outBuffer, char* inBuffer, int parameter, int maxSize)
+{
+	int cBeg;
+	int pos = 0;
+	int len;
+
+	for (int i = 1; i < parameter; i++)
+	{
+
+		while (inBuffer[pos] != ',' && inBuffer[pos] != NULL) pos++;
+		pos++;
+
+	}
+
+	while (inBuffer[pos] != '\"' && inBuffer[pos] != NULL) pos++;
+	cBeg = ++pos;
+
+	while (inBuffer[pos] != '\"' && inBuffer[pos] != NULL) pos++;
+	len = pos - cBeg;
+	if (len > maxSize) len = maxSize;
+
+
+	strncpy(outBuffer, (char*)&inBuffer[cBeg], len);
+	outBuffer[len] = 0;
+	//fprintf(errlog, "_parseString, pos: %i, cBeg: %i, deviceName: %s\n", pos, cBeg, target);
+
+	return 0;
+}
+
+int XPLPro::_parseInt(int* outTarget, char* inBuffer, int parameter)
+{
+	int cBeg;
+	int pos = 0;
+
+	for (int i = 1; i < parameter; i++)
+	{
+
+		while (inBuffer[pos] != ',' && inBuffer[pos] != NULL) pos++;
+		pos++;
+
+	}
+	cBeg = pos;
+
+	while (inBuffer[pos] != ',' && inBuffer[pos] != NULL && inBuffer[pos] != XPL_PACKETTRAILER) pos++;
+
+	char holdChar = inBuffer[pos];
+	inBuffer[pos] = 0;
+	*outTarget = atoi((char*)&inBuffer[cBeg]);
+
+	//	fprintf(errlog, "outTarget: %i cBeg: %i pos: %i string: %s\n", *outTarget, cBeg, pos, (char*)&inBuffer[cBeg]);
+
+	inBuffer[pos] = holdChar;
+
+
+
+	return 0;
+
+}
+
+
+/*
+int XPLPro::_getHandleFromFrame()			// Assuming receive buffer is holding a good frame
+{
+	char holdChar;
+	int handleRet;
+
+	holdChar = _receiveBuffer[5];
+	_receiveBuffer[5] = 0;
+	handleRet = atoi((char*)&_receiveBuffer[2]);
+	
+	_receiveBuffer[5] = holdChar;
+	return handleRet;
+
+}
+
+int XPLPro::_getPayloadFromFrame(long int *value)			// Assuming receive buffer is holding a good frame
+{
+	char holdChar;
+	
+	holdChar = _receiveBuffer[15];
+	_receiveBuffer[15] = 0;
+	*value = atol((char*)&_receiveBuffer[5]);
+	
+	_receiveBuffer[15] = holdChar;
+
+	return 0;
+
+}
+
+int XPLPro::_getPayloadFromFrame(float *value)			// Assuming receive buffer is holding a good frame
+{
+	char holdChar;
+	
+	holdChar = _receiveBuffer[15];
+	_receiveBuffer[15] = 0;
+	*value = atof((char*)&_receiveBuffer[5]);
+
+	_receiveBuffer[15] = holdChar;
+	return 0;
+
+}
+
+int XPLPro::_getPayloadFromFrame(char* value)			// Assuming receive buffer is holding a good frame
+{
+	
+	memcpy(value, (char*)&_receiveBuffer[5], _receiveBufferBytesReceived -6);
+	value[_receiveBufferBytesReceived -6] = 0;							// erase the packet trailer
+	for (int i = 0; i < _receiveBufferBytesReceived - 6; i++)
+		if (value[i] == 7) value[i] = XPL_PACKETTRAILER;			//  How I deal with the possibility of the packet trailer being within a string
+	//streamPtr->println(value);
+	return 0;
+
+}
+
+*/
+
+#ifdef XPL_USE_PROGMEM
+int XPLPro::registerDataRef(const __FlashStringHelper* datarefName, int rwmode)
+{
+	return -1;
+}
+#endif
+
+int XPLPro::registerDataRef(const char* datarefName, int rwmode)
+{
+	long int startTime;
+
+	
+
+	sprintf(_sendBuffer, "%c%c,\"%s\", %i%c", XPL_PACKETHEADER, XPLREQUEST_REGISTERDATAREF, datarefName, rwmode, XPL_PACKETTRAILER);
+	_transmitPacket();
+
+	_handleAssignment = -1;
+	startTime = millis();					// for timeout function
+	
+	while (millis() - startTime < XPL_RESPONSE_TIMEOUT && _handleAssignment<0)
+		_processSerial();
+		
+	if (_handleAssignment < 0) sendDebugMessage("registerDataRef timed out"); else	sendDebugMessage("registerDataRef successful");
+	
+	return _handleAssignment;
+	
+
+	
+}
+
+
+
+#ifdef XPL_USE_PROGMEM
+int XPLPro::registerCommand(const __FlashStringHelper* commandName)		// user will trigger commands with commandTrigger
+{
+
+	return -1;
+	
+}
+#endif
+
+int XPLPro::registerCommand(const char* commandName)		// user will trigger commands with commandTrigger
+{
+
+	long int startTime;
+
+	startTime = millis();					// for timeout function
+
+	sprintf(_sendBuffer, "%c%c,\"%s\"%c", XPL_PACKETHEADER, XPLREQUEST_REGISTERCOMMAND, commandName, XPL_PACKETTRAILER);
+	_transmitPacket();
+
+	_handleAssignment = false;
+
+	while (millis() - startTime < XPL_RESPONSE_TIMEOUT && !_handleAssignment)
+		_processSerial();
+
+	return _handleAssignment;
+	
+	
+	
+}
+
---
+++ XPLPro_Arduino/XPLPro.h
@@ -0,0 +1,151 @@
+﻿/*
+  XPLPro.h - Library for serial interface to Xplane SDK.
+  Created by Curiosity Workshop, Michael Gerlicher,  2020-2023
+
+  See readme.txt file for information on updates.
+
+  To report problems, download updates and examples, suggest enhancements or get technical support, please visit:
+     
+     discord:  https://discord.gg/gzXetjEST4
+     patreon:  www.patreon.com/curiosityworkshop
+ 
+*/
+ 
+#ifndef XPLPro_h
+#define XPLPro_h
+
+#define XPL_RX_TIMEOUT          500               // after detecting a frame header, how long will we wait to receive the rest of the frame.  (default 500 ms)
+#define XPL_RESPONSE_TIMEOUT    2000              // after sending a registration request, how long will we wait for the response.  (default 2000 ms)  
+
+#if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__) || defined (__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)        // add to this for boards that need it
+    #define XPL_USE_PROGMEM                                 // define this for boards with limited memory that can use PROGMEM to store strings.  
+                                                            // You will need to wrap your dataref names with F() macro ie:
+                                                            // Xinterface.registerDataref(F("laminar/B738/annunciator/drive2"), XPL_READ, 100, 0, &drive2);
+                                                            // Disable for boards that have issues compiling: errors with strncmp_PF for instance.
+
+#endif
+
+
+#define XPLMAX_PACKETSIZE 200                           // Probably leave this alone.  If you need a few extra bytes of RAM it could be reduced, but it needs to
+                                                        // be as long as the longest dataref name + 10.  If you are using datarefs 
+                                                        // that transfer strings it needs to be big enough for those too.  (default 200) 
+
+
+//////////////////////////////////////////////////////////////
+// STOP! Dont change any other defines in this header!
+//////////////////////////////////////////////////////////////
+
+#define XPL_BAUDRATE 115200           // don't mess with this, it needs to match the plugin which won't change
+#define XPL_PACKETHEADER  '['         // ...or this
+#define XPL_PACKETTRAILER ']'         // ...or this                                                                           
+#define XPL_VERSION 2303281           // The plugin will start to verify that a compatible version is being used
+//#define XPL_ID 0                      // Used for relabled plugins to identify the company.  0 = normal distribution version
+
+
+
+#define XPLRESPONSE_NAME           '0'          // Arduino responds with device name as initialized in the "begin" function
+#define XPLRESPONSE_DATAREF        '3'          // Plugin responds with handle to dataref or - value if not found.  dataref handle, dataref name 
+#define XPLRESPONSE_COMMAND        '4'          // Plugin responds with handle to command or - value if not found.  command handle, command name
+#define XPLRESPONSE_VERSION        'V'          // Arduino responds with build version number if requested
+#define XPLCMD_PRINTDEBUG          '1'          // plugin logs string sent from arduino
+#define XPLCMD_RESET               '2'
+#define XPLCMD_SPEAK               'S'          // plugin speaks string through xplane speech 
+#define XPLCMD_SENDNAME            'a'          // plugin request name from arduino
+#define XPLREQUEST_REGISTERDATAREF 'b'   // 
+#define XPLREQUEST_REGISTERCOMMAND 'm'  // just the name of the command to register
+#define XPLREQUEST_NOREQUESTS      'c'   // nothing to request
+#define XPLREQUEST_REFRESH         'd'	//  the plugin will call this once xplane is loaded in order to get fresh updates from arduino handles that write
+
+#define XPLCMD_DATAREFUPDATE       'e'
+#define XPLCMD_SENDREQUEST         'f'          // plugin sends this when it is ready to register bindings
+#define XPLCMD_DEVICEREADY         'g'
+#define XPLCMD_DEVICENOTREADY      'h'
+#define XPLCMD_COMMANDSTART         'i'
+#define XPLCMD_COMMANDEND           'j'
+#define XPLCMD_COMMANDTRIGGER       'k'   //  %3.3i%3.3i   command handle, number of triggers
+
+#define XPL_EXITING                 'x'     // MG 03/14/2023: xplane sends this to the arduino device during normal shutdown of xplane.  It may not happen if xplane crashes.
+
+
+#define XPL_READ		1
+#define XPL_WRITE       2
+#define XPL_READWRITE	3
+
+#define XPL_DATATYPE_INT    1
+#define XPL_DATATYPE_FLOAT  2
+#define XPL_DATATYPE_STRING 3
+
+
+
+class XPLPro
+{
+  public:
+
+    XPLPro(Stream*); 
+    void begin(char *devicename, void *initFunction, void *shutdownFunction);     // parameter is name of your device for reference 
+  
+    int connectionStatus(void);
+    int commandTrigger(int commandHandle);                                                      // triggers specified command 1 time;
+    int commandTrigger(int commandHandle, int triggerCount);                                    // triggers specified command triggerCount times.  
+    int commandStart(int commandHandle);                                                        // Avoid this unless you know what you are doing.  Command "begins" must be balanced with command "ends"
+    int commandEnd(int commandHandle);
+  //  int datarefsUpdated();                                                             // returns true if xplane has updated any datarefs since last call to datarefsUpdated()
+  //  int hasUpdated(int handle);                                                        // returns true if xplane has updated this dataref since last call to hasUpdated()
+   // void datarefWrite(int handle, long int value);                                               // write directly to variable
+   // void datarefWrite(int handle, float value);
+   // void datarefRead(int handle, long int *value);
+   // void datarefRead(int handle, float *value);
+
+
+    int registerDataRef(const char *, int);                    // single int dataref.  name, Mode, rate, divider, Value
+    int registerDataRef(const __FlashStringHelper*, int);
+    int registerCommand(const char* commandName);                                           // register a command    
+    int registerCommand(const __FlashStringHelper* commandName);                                // use this overload to trigger the command manually (commandTrigger)
+
+     
+                                                                                                //int sendReadyCommand(void);
+    //int getCurrentData(int, int*);
+    int sendDebugMessage(const char*);
+    int sendSpeakMessage(const char* msg);
+ //   int allDataRefsRegistered(void);
+   			                                       
+    void sendResetRequest(void);
+    int xloop(void);                                                                            // where the magic happens!
+  
+    
+  private:
+      void _processSerial();
+      void _processPacket();
+
+      void _sendPacketInt(int command, int handle, long int value);         // for ints
+      void _sendPacketFloat(int command, int handle, float value);       // for floats
+      void _sendPacketVoid(int command, int handle);                    // just a command with a handle
+      void _sendPacketString(int command, const char* str);                     // send a string
+   
+      void _transmitPacket();
+
+      void _sendname();
+     
+      
+      int _parseInt(int* outTarget, char* inBuffer, int parameter);
+      int _parseString(char* outBuffer, char* inBuffer, int parameter, int maxSize);
+          
+      Stream* streamPtr;
+
+    
+      char _receiveBuffer[XPLMAX_PACKETSIZE]; int _receiveBufferBytesReceived;
+      char _sendBuffer[XPLMAX_PACKETSIZE];
+  
+     
+      byte _registerFlag;
+      byte _connectionStatus;
+      
+      void (*_xplInitFunction)(void);                    // this function will be called when the plugin is ready to receive binding requests
+      void (*_xplStopFunction)(void);                       // this function will be called with the plugin receives message or detects xplane flight model inactive
+
+      const char * _deviceName;
+
+      int _handleAssignment;
+};
+
+#endif
